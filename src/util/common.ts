ee78b690898d733d762168dafcc5bcc3b43875d1
doanthai.pc <doanquocthai.it@gmail.com>
2025-05-04 11:03:05 +0700

    chore: change js to ts

--- src/util/common.ts
+++ src/util/common.ts
@@ -1,4 +1,4 @@
-export function formatViewCount(count) {
+export function formatViewCount(count: number): string {
   if (count < 1000) return `${count}`;
   if (count < 1_000_000) return `${(count / 1000).toFixed(count >= 10_000 ? 0 : 1)}K`;
   if (count < 1_000_000_000) return `${(count / 1_000_000).toFixed(1)}M`;
