3a966ea713cc391ca126da9c2c970043660d5156
doanthai.pc <doanquocthai.it@gmail.com>
2025-06-03 17:25:37 +0700

    feat: add ffmpeg script for win

--- src/api/util/index.ts
+++ src/api/util/index.ts
@@ -1,9 +1,9 @@
-// import path from "path";
-// import isDev from "electron-is-dev";
+import path from "path";
+import isDev from "electron-is-dev";
 
-// export const ffmpegPath =
-//   isDev
-//     ? path.join(__dirname, 'bin', process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg')
-//     : path.join(process.resourcesPath, 'bin', process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg');
+export const ffmpegPath =
+  isDev
+    ? path.join(__dirname, '..', '..', 'bin', process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg')
+    : path.join(process.resourcesPath, 'bin', process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg');
 
-export const ffmpegPath = 'ffmpeg';
+// export const ffmpegPath = 'ffmpeg';
