87c17b4fac81fb450b3494839eb3d86ba835774b
doanthai.pc <doanquocthai.it@gmail.com>
2025-06-03 12:05:31 +0700

    chore: refactor api craw data

--- src/api/events/version.ts
+++ src/api/events/version.ts
@@ -3,6 +3,7 @@ import { IPCEvent } from 'src/util/constant';
 import { getSettings } from '../dal/setting';
 import { TranscriptRequest } from 'src/util/dto';
 
+
 async function getVideoInformation(creatorId: string, videoId: string, cookies: string): Promise<Record<string, any>> {
   const videoInfo = await fetch(`https://www.tiktok.com/@${creatorId}/video/${videoId}`, {
     method: 'GET',
@@ -68,6 +69,82 @@ export async function getTranscript(creatorId: string, videoId: string, musicURL
   }
 }
 
+export async function searchTiktok(search: string, cookies: string): Promise<any> {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: '*/*',
+      'accept-language': 'en-US,en;q=0.8',
+      priority: 'u=1, i',
+      'sec-ch-ua': '"Chromium";v="136", "Brave";v="136", "Not.A/Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Linux"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-origin',
+      'sec-gpc': '1',
+      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+      Cookie: cookies
+    }
+  };
+  const params = new URLSearchParams({
+    WebIdLastTime: Date.now().toString(),
+    aid: '1988',
+    app_language: 'en',
+    app_name: 'tiktok_web',
+    browser_language: 'en-US',
+    browser_name: 'Mozilla',
+    browser_online: 'true',
+    browser_platform: 'Linux x86_64',
+    browser_version: '5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+    channel: 'tiktok_web',
+    cookie_enabled: 'true',
+    data_collection_enabled: 'true',
+    device_id: '7497448105643329031',
+    device_platform: 'web_pc',
+    device_type: 'web_h264',
+    focus_state: 'true',
+    from_page: 'search',
+    history_len: '3',
+    is_fullscreen: 'false',
+    is_page_visible: 'true',
+    keyword: search,
+    odinId: '7497447860569588754',
+    os: 'linux',
+    region: 'VN',
+    screen_height: '1050',
+    screen_width: '1680',
+    search_source: 'normal_search',
+    tz_name: 'Asia/Saigon',
+    user_is_login: 'false',
+    web_search_code: '{"tiktok":{"client_params_x":{"search_engine":{"ies_mt_user_live_video_card_use_libra":1,"mt_search_general_user_live_card":1}},"search_server":{}}}',
+    webcast_language: 'en',
+  });
+  let url = 'https://www.tiktok.com/api/search/general/full/?' + params.toString();
+  const result: any[] = [];
+  let json: any = null;
+  do {
+    console.count('Page');
+    url = 'https://www.tiktok.com/api/search/general/full/?' + params.toString();
+    const res = await fetch(url, options);
+    json = await res.json();
+    const msToken = res.headers.get('set-cookie') || '';
+    params.set('focus_state', 'false');
+    params.set('msToken', msToken.match(/msToken=([^;]+)/)[1]);
+    params.set('offset', json?.cursor || '0');
+    params.set('search_id', json?.extra?.logid || '');
+    options.headers.Cookie = cookies + '; ' + msToken;
+    json?.data?.forEach(({ type, item }: any) => {
+      if (type !== 1) return;
+      result.push(item)
+    });
+  } while (json?.has_more === 1)
+  return {
+    success: true,
+    data: result,
+  };
+}
+
 const initialize = (_: BrowserWindow) => {
   ipcMain.handle(IPCEvent.GET_VERSION, async (_) => {
     return app.getVersion();
@@ -79,83 +156,11 @@ const initialize = (_: BrowserWindow) => {
     return getTranscript(creatorId, videoId, musicURL, cookies);
   });
 
-  ipcMain.handle(IPCEvent.CRAWLER_VIDEO, async (_, { search }) => {
+  ipcMain.handle(IPCEvent.CRAWLER_VIDEO, async (_, { search, type }) => {
     const settings = getSettings();
     const cookies = settings.tiktokCookies;
     try {
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: '*/*',
-          'accept-language': 'en-US,en;q=0.8',
-          priority: 'u=1, i',
-          'sec-ch-ua': '"Chromium";v="136", "Brave";v="136", "Not.A/Brand";v="99"',
-          'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"Linux"',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-origin',
-          'sec-gpc': '1',
-          'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
-          Cookie: cookies
-        }
-      };
-      const params = new URLSearchParams({
-        WebIdLastTime: Date.now().toString(),
-        aid: '1988',
-        app_language: 'en',
-        app_name: 'tiktok_web',
-        browser_language: 'en-US',
-        browser_name: 'Mozilla',
-        browser_online: 'true',
-        browser_platform: 'Linux x86_64',
-        browser_version: '5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
-        channel: 'tiktok_web',
-        cookie_enabled: 'true',
-        data_collection_enabled: 'true',
-        device_id: '7497448105643329031',
-        device_platform: 'web_pc',
-        device_type: 'web_h264',
-        focus_state: 'true',
-        from_page: 'search',
-        history_len: '3',
-        is_fullscreen: 'false',
-        is_page_visible: 'true',
-        keyword: search,
-        odinId: '7497447860569588754',
-        os: 'linux',
-        region: 'VN',
-        screen_height: '1050',
-        screen_width: '1680',
-        search_source: 'normal_search',
-        tz_name: 'Asia/Saigon',
-        user_is_login: 'false',
-        web_search_code: '{"tiktok":{"client_params_x":{"search_engine":{"ies_mt_user_live_video_card_use_libra":1,"mt_search_general_user_live_card":1}},"search_server":{}}}',
-        webcast_language: 'en',
-      });
-      let url = 'https://www.tiktok.com/api/search/general/full/?' + params.toString();
-      const result: any[] = [];
-      let json = null;
-      do {
-        console.count('Page');
-        url = 'https://www.tiktok.com/api/search/general/full/?' + params.toString();
-        const res = await fetch(url, options);
-        json = await res.json();
-        const msToken = res.headers.get('set-cookie') || '';
-        params.set('focus_state', 'false');
-        params.set('msToken', msToken.match(/msToken=([^;]+)/)[1]);
-        params.set('offset', json?.cursor || '0');
-        params.set('search_id', json?.extra?.logid || '');
-        options.headers.Cookie = cookies + '; ' + msToken;
-        json?.data?.forEach(({ type, item }: any) => {
-          if (type !== 1) return;
-          result.push(item)
-        });
-      } while (json?.has_more === 1)
-      return {
-        success: true,
-        data: result,
-      };
+      return searchTiktok(search, cookies);
     } catch (error) {
       console.error('Error fetching TikTok videos:', error);
       return {
