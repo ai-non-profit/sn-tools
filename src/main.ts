d40fc4eb437ec514627d7846e59ea9da28e05241
doanthai.pc <doanquocthai.it@gmail.com>
2025-08-03 17:35:06 +0700

    bug: fix bug load video tiktok in new secure with Referer

--- src/main.ts
+++ src/main.ts
@@ -102,8 +102,12 @@ const createWindow = () => {
       session: undefined // Optional: use Electron session
     });
 
+    log.info(settings.tiktokCookies);
+
     // Set any cookies or headers required for auth
     clientRequest.setHeader('Cookie', settings.tiktokCookies);
+    clientRequest.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+    clientRequest.setHeader('referer', 'https://www.tiktok.com/');
     // clientRequest.setHeader('Authorization', 'Bearer ...');
 
     clientRequest.on('response', (res) => {
